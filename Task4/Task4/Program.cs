0b142197b7210ade2a639fef9ffdc41b216b22d2
Maksim <xSeptentrioNx@gmail.com>
2020-02-09 22:06:22 +0300

    Version 2.0

--- Task4/Task4/Program.cs
+++ Task4/Task4/Program.cs
@@ -8,16 +8,20 @@ namespace Task4
         {
             Console.Write("Введите число: ");
             string number = Console.ReadLine();
-            if (ulong.TryParse(number, out ulong temp))
+
+            if (ulong.TryParse(number, out ulong inputValue))
             {
                 char[] arr = number.ToCharArray();
                 Console.Write("\nВведенное число наоборот: ");
-                for (int i = arr.Length - 1 ; i >= 0; i--)
+                for (int i = arr.Length - 1; i >= 0; i--)
                 {
                     Console.Write(arr[i]);
                 }
             }
-            else Console.WriteLine("Значение введено неверно.");
+            else
+            {
+                Console.WriteLine("Значение введено неверно.");
+            }
             Console.ReadKey(true);
         }
     }
